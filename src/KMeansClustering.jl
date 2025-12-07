2e912acfe44eab2f4074aeafe73886d532a01458
Shady <shadypost1@gmail.com>
2025-12-07 16:06:52 +0100

    Add KMeansClustering module and kmeans API stub

--- src/KMeansClustering.jl
+++ src/KMeansClustering.jl
@@ -1,5 +1,39 @@
 module KMeansClustering
 
-# Write your package code here.
+using Random
 
+include("types.jl")
+include("utils.jl")
+include("algorithms/kmeans.jl")
+include("algorithms/kmeanspp.jl")
+include("algorithms/bkmeans.jl")
+include("algorithms/ckmeans.jl")
+
+export kmeans, KMeansResult
+
+"""
+    kmeans(X, k; init=:random, maxiter=100, tol=1e-4, rng=Random.GLOBAL_RNG)
+
+High-level entry point for k-means clustering.
+
+Arguments
+- X: data matrix with features in rows and observations in columns.
+- k: number of clusters.
+
+Keyword arguments
+- init: initialization strategy (:random, :kmeanspp).
+- maxiter: maximum number of Lloyd iterations.
+- tol: tolerance for convergence.
+- rng: random number generator.
+
+Returns a `KMeansResult`.
+"""
+function kmeans(X::AbstractMatrix{<:Real}, k::Integer;
+                init::Symbol = :random,
+                maxiter::Int = 100,
+                tol::Real = 1e-4,
+                rng::AbstractRNG = Random.GLOBAL_RNG)
+    error("kmeans is not implemented yet. Implement Lloyd's algorithm in algorithms/kmeans.jl and call it from here.")
 end
+
+end # module
