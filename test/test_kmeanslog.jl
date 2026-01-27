292f7aa38b087d8a82b025b44114334be1112707
migraine-user <mj9012159@gmail.com>
2026-01-27 15:35:04 +0100

    add one sanity check

--- test/test_kmeanslog.jl
+++ test/test_kmeanslog.jl
@@ -2,6 +2,23 @@ using Test
 using Random
 using KMeansClustering
 
+@testset "kmeanslog: number of centers should be the same as the initial centroids" begin
+  # Generate sample data (features in rows, observations in columns)
+  Random.seed!(42)
+  X = randn(2, 100)  # 2 features, 100 observations
+
+  # Perform k-means clustering with 3 clusters
+  k = 3
+  idx = randperm(Random.default_rng(), size(X, 2))[1:k]
+  init = :random
+  maxiter = 100
+  maxinneriter = 100
+
+  # tol = 1
+  converged = KMeansClustering.kmeanslog(X, X[:, idx], init, maxiter=maxiter, maxinneriter=maxinneriter, 1)
+  @test size(converged.centers, 2) == k
+end
+
 @testset "kmeanslog: inertia should be smaller if tolerance is much smaller and does not converge" begin
   # Generate sample data (features in rows, observations in columns)
   Random.seed!(42)
